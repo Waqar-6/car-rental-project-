dbf60283f255cd3c8fe797859ca746ea0099f330
Waqar <waqar-1@hotmail.co.uk>
2024-12-28 02:02:20 +0000

    chore: created project using vite and spring initalzr

---
+++ car-rental-backend/src/main/java/com/w_farooq_group/CarRentalBackendApplication.java
@@ -0,0 +1,13 @@
+package com.w_farooq_group;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarRentalBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarRentalBackendApplication.class, args);
+	}
+
+}
---
+++ car-rental-backend/src/test/java/com/w_farooq_group/CarRentalBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.w_farooq_group;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarRentalBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
