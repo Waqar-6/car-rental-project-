38c2da9d4faac83a9129613d49153923f6fc6ea2
Waqar <waqar-1@hotmail.co.uk>
2024-12-28 14:27:36 +0000

    feat(repository): add jpa repository for vehicle entities

---
+++ car-rental-backend/src/main/java/com/w_farooq_group/repository/VehicleRepository.java
@@ -0,0 +1,11 @@
+package com.w_farooq_group.repository;
+
+import com.w_farooq_group.entity.Vehicle;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface VehicleRepository extends JpaRepository<Vehicle, UUID> {
+}
