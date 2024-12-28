0098ddc867d442f98a305a26aeec7c3947e85c9c
Waqar <waqar-1@hotmail.co.uk>
2024-12-28 15:44:36 +0000

    feat(mapper): create methods to convert entity to dtos and dtos to entity

---
+++ car-rental-backend/src/main/java/com/w_farooq_group/mapper/VehicleMapper.java
@@ -0,0 +1,66 @@
+package com.w_farooq_group.mapper;
+
+import com.w_farooq_group.dto.CarDto;
+import com.w_farooq_group.dto.VanDto;
+import com.w_farooq_group.dto.VehicleDto;
+import com.w_farooq_group.entity.Car;
+import com.w_farooq_group.entity.Van;
+import com.w_farooq_group.entity.Vehicle;
+
+public final class VehicleMapper {
+
+    private VehicleMapper () {}
+
+
+    // Entity to Dto
+
+    public static VehicleDto mapToVehicleDto (Vehicle vehicle, VehicleDto vehicleDto) {
+        vehicleDto.setBrand(vehicle.getBrand());
+        vehicleDto.setModel(vehicle.getModel());
+        vehicleDto.setReg(vehicle.getReg());
+        vehicleDto.setDailyRate(vehicle.getDailyRate());
+        vehicleDto.setAvailable(vehicle.isAvailable());
+        vehicleDto.setFuelType(vehicle.getFuelType());
+        return vehicleDto;
+    }
+
+    public static CarDto mapToCarDto (Car car, CarDto carDto) {
+        mapToVehicleDto(car, carDto);
+        carDto.setNumDoors(car.getNumDoors());
+        carDto.setClassCategory(car.getClassCategory());
+        return carDto;
+    }
+
+    public static VanDto mapToVanDto (Van van, VanDto vanDto) {
+        mapToVehicleDto(van, vanDto);
+        vanDto.setHasTailLift(van.isHasTailLift());
+        vanDto.setWeightLimit(van.getWeightLimit());
+        return vanDto;
+    }
+
+    // Dto to Entity
+
+    public static Vehicle mapToVehicle (VehicleDto vehicleDto, Vehicle vehicle) {
+        vehicle.setBrand(vehicleDto.getBrand());
+        vehicle.setModel(vehicleDto.getModel());
+        vehicle.setReg(vehicleDto.getReg());
+        vehicle.setDailyRate(vehicleDto.getDailyRate());
+        vehicle.setAvailable(vehicleDto.isAvailable());
+        vehicle.setFuelType(vehicleDto.getFuelType());
+        return vehicle;
+    }
+
+    public static Car mapToCar (CarDto carDto, Car car) {
+        mapToVehicle(carDto, car);
+        car.setNumDoors(carDto.getNumDoors());
+        car.setClassCategory(carDto.getClassCategory());
+        return car;
+    }
+
+    public static Van mapToVan (VanDto vanDto, Van van) {
+        mapToVehicle(vanDto, van);
+        van.setHasTailLift(vanDto.isHasTailLift());
+        van.setWeightLimit(vanDto.getWeightLimit());
+        return van;
+    }
+}
