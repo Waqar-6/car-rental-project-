ce84282036099ee8936c0979d9016ae745e5f695
Waqar <waqar-1@hotmail.co.uk>
2025-01-19 21:24:32 +0000

    chore: add logging for vehicle controller

--- car-rental-backend/src/main/java/com/w_farooq_group/vehicle/controller/VehicleController.java
+++ car-rental-backend/src/main/java/com/w_farooq_group/vehicle/controller/VehicleController.java
@@ -6,6 +6,8 @@ import com.w_farooq_group.shared.dto.ResponseDto;
 import com.w_farooq_group.vehicle.dto.VanDto;
 import com.w_farooq_group.vehicle.dto.VehicleDto;
 import com.w_farooq_group.vehicle.service.IVehicleService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -18,6 +20,8 @@ import java.util.List;
 @RequestMapping(value = "/api/v1/vehicles", produces = {MediaType.APPLICATION_JSON_VALUE})
 public class VehicleController {
 
+    private static final Logger log = LoggerFactory.getLogger(VehicleController.class);
+
     private final IVehicleService iVehicleService;
 
     public VehicleController(IVehicleService iVehicleService) {
@@ -27,6 +31,7 @@ public class VehicleController {
     @PreAuthorize("hasRole('ADMIN')")
     @PostMapping("/car/create")
     public ResponseEntity<ResponseDto> createCar (@RequestParam String type, @RequestBody CarDto carDto) {
+        log.info("new Car create request with reg :  {}", carDto.getReg());
         iVehicleService.createVehicle(type, carDto);
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(new ResponseDto(AppConstants.STATUS_201, AppConstants.MESSAGE_201));
@@ -35,6 +40,7 @@ public class VehicleController {
     @PreAuthorize("hasRole('ADMIN')")
     @PostMapping("/van/create")
     public ResponseEntity<ResponseDto> createVan (@RequestParam String type, @RequestBody VanDto vanDto) {
+        log.info("new Van create request with reg :  {}", vanDto.getReg());
         iVehicleService.createVehicle(type, vanDto);
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(new ResponseDto(AppConstants.STATUS_201, AppConstants.MESSAGE_201));
@@ -43,6 +49,7 @@ public class VehicleController {
 
     @GetMapping("/fetch/{reg}")
     public ResponseEntity<?> fetchVehicleByReg (@PathVariable String reg) {
+        log.info("Vehicle with reg {} fetch request", reg);
         VehicleDto vehicleDto = iVehicleService.fetchVehicleByReg(reg);
         return ResponseEntity.status(HttpStatus.OK).body(vehicleDto);
     }
@@ -50,6 +57,7 @@ public class VehicleController {
     @PreAuthorize("hasRole('ADMIN')")
     @PutMapping("/update/{reg}")
     public ResponseEntity<ResponseDto> updateVehicle (@PathVariable String reg, @RequestBody VehicleDto vehicleDto) {
+        log.info("vehicle with reg {} update request", vehicleDto.getReg());
         boolean isUpdated = iVehicleService.updateVehicle(reg, vehicleDto);
         return isUpdated ? ResponseEntity.status(HttpStatus.OK).body(new ResponseDto(AppConstants.STATUS_200, AppConstants.STATUS_200)) :
                 ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body(new ResponseDto(AppConstants.STATUS_417, AppConstants.MESSAGE_417_UPDATE));
@@ -57,7 +65,8 @@ public class VehicleController {
 
     @PreAuthorize("hasRole('ADMIN')")
     @PutMapping("/delete/{reg}")
-    public ResponseEntity<ResponseDto> updateVehicle (@PathVariable String reg) {
+    public ResponseEntity<ResponseDto> deleteVehicle (@PathVariable String reg) {
+        log.info("delete request for vehicle with reg {}", reg);
         boolean isDeleted = iVehicleService.deleteVehicle(reg);
         return isDeleted ? ResponseEntity.status(HttpStatus.OK).body(new ResponseDto(AppConstants.STATUS_200, AppConstants.STATUS_200)) :
                 ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body(new ResponseDto(AppConstants.STATUS_417, AppConstants.MESSAGE_417_DELETE));
@@ -65,6 +74,7 @@ public class VehicleController {
 
     @GetMapping("/fetch/cars")
     public ResponseEntity<List<CarDto>> fetchAllCars () {
+        log.info("fetch request for all cars");
         List<CarDto> allCars = iVehicleService.getAllCars();
         return ResponseEntity.status(HttpStatus.OK).body(allCars);
     }
--- car-rental-backend/src/test/java/com/w_farooq_group/vehicle/service/impl/VehicleServiceImplTest.java
+++ car-rental-backend/src/test/java/com/w_farooq_group/vehicle/service/impl/VehicleServiceImplTest.java
@@ -1,13 +1,44 @@
 package com.w_farooq_group.vehicle.service.impl;
 
+import com.w_farooq_group.vehicle.dto.CarDto;
+import com.w_farooq_group.vehicle.dto.VehicleDto;
+import com.w_farooq_group.vehicle.entity.Car;
+import com.w_farooq_group.vehicle.entity.Vehicle;
+import com.w_farooq_group.vehicle.factory.VehicleFactory;
+import com.w_farooq_group.vehicle.mapper.VehicleMapper;
+import com.w_farooq_group.vehicle.repository.VehicleRepository;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 class VehicleServiceImplTest {
 
+    
     @Test
-    void createVehicle() {
+    void testCreateVehicle_whenGivenCarDto_shouldCreateCar() {
+
+        // Arrange
+        VehicleRepository vehicleRepository = Mockito.mock(VehicleRepository.class);
+        VehicleFactory vehicleFactory = Mockito.mock(VehicleFactory.class);
+        CarDto carDto = new CarDto(); carDto.setBrand("BMW"); carDto.setModel("5 series"); carDto.setReg("bmw22 pff"); carDto.setFuelType("petrol"); carDto.setDailyRate(120.20);
+        carDto.setAvailable(true);
+        carDto.setNumDoors((byte) 2);
+        carDto.setClassCategory("SEDAN");
+
+        VehicleServiceImpl vehicleService = new VehicleServiceImpl(vehicleRepository, vehicleFactory);
+        Vehicle car = VehicleMapper.mapToCar(carDto, new Car());
+
+        when(vehicleRepository.existsByReg(carDto.getReg())).thenReturn(false);
+
+        when(vehicleFactory.createVehicle(Mockito.anyString(), Mockito.any(VehicleDto.class))).thenReturn(car);
+
+        vehicleService.createVehicle("car", carDto);
+        verify(vehicleRepository).existsByReg("bmw22 pff");
+
+        vehicleRepository.save(car);
     }
 
     @Test
